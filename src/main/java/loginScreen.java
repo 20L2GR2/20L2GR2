ceb06a25bc18fbfc10155224fbfe345372f08d9f
AndrzejBieda <andrzej.b116@gmail.com>
2020-04-03 21:04:36 +0200

    projekt z jednym widokiem

---
+++ src/main/java/loginScreen.java
@@ -0,0 +1,37 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import jfxtras.styles.jmetro.JMetro;
+import jfxtras.styles.jmetro.Style;
+
+import java.io.IOException;
+
+public class loginScreen extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/views/application.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Scene scene = new Scene(root, 1600, 800);
+
+        stage.setTitle("AutoService");
+        stage.setScene(scene);
+        stage.show();
+        JMetro jMetro = new JMetro(Style.DARK);
+        jMetro.setAutomaticallyColorPanes(true);
+        jMetro.setScene(scene);
+        root.setStyle("-fx-font: title");
+
+    }
+}
