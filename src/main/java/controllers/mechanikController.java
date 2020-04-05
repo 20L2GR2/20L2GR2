fcaf06475028030f97cfb48d41e82ec626db2f99
Wiktor <cwynar48@gmail.com>
2020-04-05 21:28:06 +0200

    Interakcje widoku mechanika

---
+++ src/main/java/controllers/mechanikController.java
@@ -0,0 +1,61 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class mechanikController implements Initializable {
+
+    @FXML
+    private BorderPane borderPane;
+    @FXML
+    private Pane czesciPane,zleceniaPane,mojeZleceniaPane,profilPane;
+    @FXML
+    private ToggleButton toggleButtonCzesci, toggleButtonZlecenia, toggleButtonTwojeZlecenia, toggleButtonProfil;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        borderPane.setCenter(profilPane);
+        toggleButtonProfil.setSelected(true);
+    }
+
+    public void otworzCzesci() {
+        System.out.println("otworzCzesci");
+        borderPane.setCenter(czesciPane);
+        toggleButtonCzesci.setSelected(true);
+    }
+
+    public void otworzZlecenia() {
+        System.out.println("otworzZlecenia");
+        borderPane.setCenter(zleceniaPane);
+        toggleButtonZlecenia.setSelected(true);
+    }
+
+    public void otworzTwojeZlecenia() {
+        System.out.println("otworzTwojeZlecenia");
+        borderPane.setCenter(mojeZleceniaPane);
+        toggleButtonTwojeZlecenia.setSelected(true);
+    }
+
+    public void otworzProfil() {
+        System.out.println("otworzProfil");
+        borderPane.setCenter(profilPane);
+        toggleButtonProfil.setSelected(true);
+    }
+
+    public void zlecenieRezerwacja() {
+        System.out.println("Zarezerwowano!");
+    }
+
+    public void zlecenieZakoncz() {
+        System.out.println("Zakończono!");
+    }
+
+    public void czesciWyslij() {
+        System.out.println("Wysłano!");
+    }
+}
--- src/main/java/loginScreen.java
+++ src/main/java/loginScreen.java
@@ -18,7 +18,7 @@ public class loginScreen extends Application {
     public void start(Stage stage) {
         Parent root = null;
         try {
-            root = FXMLLoader.load(getClass().getResource("/views//views/application.fxml"));
+            root = FXMLLoader.load(getClass().getResource("/views/application.fxml"));
         } catch (IOException e) {
             e.printStackTrace();
         }
