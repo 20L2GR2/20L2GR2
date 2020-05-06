47b3ce3f631418c4452b5fce7074c7d1d2bf6213
adrianalbrycht <48263140+adrianalbrycht@users.noreply.github.com>
2020-05-06 19:02:05 +0200

    Aktualizacja struktury bazy

--- dump.sql
+++ dump.sql
@@ -1,4 +1,4 @@
-﻿INSERT INTO `klienci` (`id_klienta`, `imie_klienta`, `nazwisko_klienta`, `nr_kontaktowy`, `marka_samochodu`, `model_samochodu`, `numer_rejestracyjny`) VALUES
+INSERT INTO `klienci` (`id_klienta`, `imie_klienta`, `nazwisko_klienta`, `nr_kontaktowy`, `marka_samochodu`, `model_samochodu`, `numer_rejestracyjny`) VALUES
 (1, 'Adrian', 'Bieda', 123123123, 'Fiat', 'Linea', 'RBR-0000'),
 (2, 'Andrzej', 'Albrycht', 321321321, 'Renault', 'Scenic', 'RKR-0000'),
 (3, 'Wiktor', 'Borek', 456456456, 'Seat', 'Cordoba', 'RKR-0001'),
@@ -11,7 +11,7 @@ INSERT INTO `magazyn` (`id_czesci`, `nazwa_czesci`, `opis_czesci`, `ilosc`, `cen
 
 INSERT INTO `pracownicy` (`id_pracownika`, `imie`, `nazwisko`, `stanowisko`, `login`, `haslo`) VALUES
 (1, 'Jan', 'Kowalski', 0, 'admin', 'admin'),
-(2, 'PaweĹ‚', 'Nowak', 1, 'obslugaklienta1', 'obslugaklienta1'),
+(2, 'Paweł‚', 'Nowak', 1, 'obslugaklienta1', 'obslugaklienta1'),
 (3, 'Maciej', 'Solejuk', 2, 'mechanik1', 'mechanik1'),
 (4, 'Piotr', 'Nowakowski', 3, 'magazynier1', 'magazynier1');
 
@@ -19,5 +19,5 @@ INSERT INTO `zamowienia` (`id_zamowienia`, `nazwa_czesci`, `kometarz`, `stan_zam
 (1, 'Belka tylna Renault Scenic', 'Wymagany osprzęt układu hamulcowego', 0, 2),
 (2, 'Reflektor przedni prawy Fiat Panda', '', 0, 2);
 
-INSERT INTO `zlecenia` (`id_zlecenie`, `id_klienta`, `id_mechanika`, `id_obslugaklientastart`, `id_obslugaklientakoniec`, `opis_usterki`, `data_rozpoczecia`, `data_zakonczenia`, `opis_naprawy`, `uzyte_czesci`, `cena`) VALUES
-(1, 1, NULL, 3, NULL, 'Wymiana klockĂłw hamulcowych przód', '2020-04-19 10:00:00', NULL, NULL, NULL, NULL);
+INSERT INTO `zlecenia` (`id_zlecenie`, `id_klienta`, `id_mechanika`, `id_obslugaklientastart`, `id_obslugaklientakoniec`, `stan_zlecenia`, `opis_usterki`, `data_rozpoczecia`, `data_zakonczenia`, `opis_naprawy`, `uzyte_czesci`, `cena`) VALUES
+(1, 1, NULL, 3, NULL, 2, 'Wymiana klocków hamulcowych przód', '2020-04-19 10:00:00', NULL, NULL, NULL, NULL);
