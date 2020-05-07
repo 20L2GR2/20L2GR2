609e7f27e08af5847b49358efea1cb751943e0a5
AndrzejBieda <andrzej.b116@gmail.com>
2020-05-07 02:04:42 +0200

    ABBCK-40 # utworzenie branch dla mechanika; dodanie częściowego wyświetlania informacji dla mechanika

--- src/main/java/main/controllers/MechanikController.java
+++ src/main/java/main/controllers/MechanikController.java
@@ -1,14 +1,26 @@
 package main.controllers;
 
+import hibernate.entity.Magazyn;
+import hibernate.entity.Pracownicy;
+import hibernate.entity.Zamowienia;
+import hibernate.util.HibernateUtil;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.util.converter.FloatStringConverter;
+import javafx.util.converter.LongStringConverter;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MechanikController implements Initializable {
@@ -20,11 +32,14 @@ public class MechanikController implements Initializable {
     private Pane czesciPane, zleceniaPane, mojeZleceniaPane, profilPane;
     @FXML
     private ToggleButton toggleButtonCzesci, toggleButtonZlecenia, toggleButtonTwojeZlecenia, toggleButtonProfil;
+    @FXML
+    public Label imieLabel, nazwiskoLabel, loginLabel;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         borderPane.setCenter(profilPane);
         toggleButtonProfil.setSelected(true);
+        inicjalizujWidokMechanikaZBazy();
     }
 
     public void logout(ActionEvent event) throws IOException {
@@ -65,5 +80,47 @@ public class MechanikController implements Initializable {
 
     public void czesciWyslij() {
         System.out.println("Wysłano!");
+    }
+
+
+    private void updateData(Object object) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            System.out.println("Update...");
+            session.update(object);
+            session.getTransaction().commit();
+            System.out.println("Updated");
+            session.clear();
+            session.disconnect();
+            session.close();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public void inicjalizujWidokMechanikaZBazy() {
+
+
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+
+            Pracownicy user = (Pracownicy) session.createQuery("FROM Pracownicy U WHERE U.idPracownika = :id").setParameter("id", LogowanieController.userID).uniqueResult();
+
+            imieLabel.setText(user.getImie());
+            nazwiskoLabel.setText(user.getNazwisko());
+            loginLabel.setText(user.getLogin());
+
+            session.clear();
+            session.disconnect();
+            session.close();
+
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }
     }
 }
