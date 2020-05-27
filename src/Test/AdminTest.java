2e4a743e9fb807a1c32e64f348bb1248c9be31c2
test <pawelkluk12@interia.pl>
2020-05-27 21:41:59 +0200

    TESTOWE

--- src/Test/AdminTest.java
+++ src/Test/AdminTest.java
@@ -1,6 +1,7 @@
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import main.controllers.AdminController;
@@ -9,6 +10,7 @@ import org.datafx.controller.flow.FlowHandler;
 import org.datafx.controller.flow.FlowView;
 import org.junit.Test;
 import org.testfx.api.FxRobotException;
+import org.testfx.assertions.api.Assertions;
 import org.testfx.framework.junit.ApplicationTest;
 
 
@@ -16,11 +18,13 @@ import org.testfx.framework.junit.ApplicationTest;
 public class AdminTest extends ApplicationTest {
 
     AdminController controller;
+    Scene scene;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent mainNode = FXMLLoader.load(getClass().getResource("/views/application.fxml"));
-        primaryStage.setScene(new Scene(mainNode));
+        scene = new Scene(mainNode);
+        primaryStage.setScene(scene);
         primaryStage.show();
         primaryStage.toFront();
     }
@@ -33,6 +37,13 @@ public class AdminTest extends ApplicationTest {
     @Test(expected = FxRobotException.class)
     public void clickOnNormalElement(){
         clickOn("#zalogujButton");
+    }
+
+    @Test
+    public void loggingWithWrongCredentialsWontAllowLogginIn(){
+        clickOn("#zalogujButton");
+        Label label = (Label) scene.lookup("#bladLogowaniaLabel");
+        Assertions.assertThat(label).isVisible();
     }
 
 
