6d9c52da1831cef8ff28263d1f7e9313b9820864
adrianalbrycht <trustek@o2.pl>
2020-06-06 18:54:11 +0200

    loading from dump

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,9 +7,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import jfxtras.styles.jmetro.JMetro;
 import jfxtras.styles.jmetro.Style;
-import pdf.GeneratePdf;
+import org.apache.ibatis.jdbc.ScriptRunner;
+//import pdf.GeneratePdf;
 
-import java.io.IOException;
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Date;
 
 public class Main extends Application {
@@ -19,12 +23,12 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage stage) {
-        //GENEROWANIE PDF odbywa się za pomocą tej klasy
-        GeneratePdf pdf = new GeneratePdf();
-        String[][] koszta = {{"Naprawa silnika", "200.50"},{"Szpachla", "100.75"}};
-        pdf.generatePDF("pdf/pdf.pdf", new Date().toString(),"Dobra-Firma", "Roafał Kowalski", "Warszawa szkolna 2", koszta);
-        //
+    public void start(Stage stage) throws SQLException, FileNotFoundException {
+//        //GENEROWANIE PDF odbywa się za pomocą tej klasy
+//        GeneratePdf pdf = new GeneratePdf();
+//        String[][] koszta = {{"Naprawa silnika", "200.50"},{"Szpachla", "100.75"}};
+//        pdf.generatePDF("pdf/pdf.pdf", new Date().toString(),"Dobra-Firma", "Roafał Kowalski", "Warszawa szkolna 2", koszta);
+//        //
 
         Parent root = null;
         try {
@@ -43,6 +47,20 @@ public class Main extends Application {
         jMetro.setAutomaticallyColorPanes(true);
         jMetro.setScene(scene);
         root.setStyle("-fx-font: title");
+
+        //Registering the Driver
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        //Getting the connection
+        String mysqlUrl = "jdbc:mysql://localhost/20l2gr2";
+        Connection con = DriverManager.getConnection(mysqlUrl, "root", "root");
+        System.out.println("Connection established......");
+        //Initialize the script runner
+        ScriptRunner sr = new ScriptRunner(con);
+        //Creating a reader object
+        Reader reader = new BufferedReader(new FileReader("dump.sql"));
+        //Running the script
+        sr.runScript(reader);
+
     }
 
 
