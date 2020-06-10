2212a49168cbe7471ae97a70989d8126c10cae48
adrianalbrycht <48263140+adrianalbrycht@users.noreply.github.com>
2020-06-10 11:34:31 +0200

    Aktualizacja dumpa

--- dump.sql
+++ dump.sql
@@ -1,8 +1,9 @@
-INSERT INTO `klienci` (`id_klienta`, `imie_klienta`, `nazwisko_klienta`, `nr_kontaktowy`, `marka_samochodu`, `model_samochodu`, `numer_rejestracyjny`) VALUES
-                                                                                                                                                           (1, 'Adrian', 'Bieda', 123123123, 'Fiat', 'Linea', 'RBR-0000'),
-                                                                                                                                                           (2, 'Andrzej', 'Albrycht', 321321321, 'Renault', 'Scenic', 'RKR-0000'),
-                                                                                                                                                           (3, 'Wiktor', 'Borek', 456456456, 'Seat', 'Cordoba', 'RKR-0001'),
-                                                                                                                                                           (4, 'Kamil', 'Cwynar', 654654654, 'Citroen', 'Saxo', 'RKR-0002');
+INSERT INTO `klienci` (`id_klienta`, `imie_klienta`, `nazwisko_klienta`, `nr_kontaktowy`, `marka_samochodu`,
+                       `model_samochodu`, `numer_rejestracyjny`)
+VALUES (1, 'Adrian', 'Bieda', 123123123, 'Fiat', 'Linea', 'RBR-0000'),
+       (2, 'Andrzej', 'Albrycht', 321321321, 'Renault', 'Scenic', 'RKR-0000'),
+       (3, 'Wiktor', 'Borek', 456456456, 'Seat', 'Cordoba', 'RKR-0001'),
+       (4, 'Kamil', 'Cwynar', 654654654, 'Citroen', 'Saxo', 'RKR-0002');
 
 INSERT INTO `magazyn` (`id_czesci`, `nazwa_czesci`, `opis_czesci`, `ilosc`, `cena`)
 VALUES (1, 'Świeca iskrowa', 'Świeca iskrowa do silników benzynowych', 100, 29.99),
@@ -26,7 +27,11 @@ VALUES (1, 1, NULL, 2, NULL, 0, 'Wymiana klocków hamulcowych przód', '2020-04-
        (2, 2, NULL, 2, NULL, 0, 'Wymiana belki tylniej', '2020-04-19 12:00:00', NULL, NULL, NULL, NULL),
        (3, 3, 3, 2, NULL, 1, 'Wymiana oleju w skrzyni biegów', '2020-04-20 08:00:00', NULL, NULL, NULL, NULL),
        (4, 4, 3, 2, NULL, 1, 'Naprawa manetki kierunkowskazów', '2020-04-20 11:00:00', NULL, NULL, NULL, NULL),
-(5, 4, 3, 2, NULL, 2, 'Niedziałający klakson', '2020-04-22 15:00:00', NULL, 'Naprawiono klakson', 'Olej silnikowy 5W40 5L - 250\nOlej silnikowy 5W40 5L - 250\nOlej silnikowy 5W40 5L - 250\n', NULL),
-(6, 3, 3, 2, NULL, 2, 'Wymiana oleju silnikowego', '2020-04-28 13:00:00', NULL, 'Wymieniono olej silnikowy', '', NULL),
-(7, 2, 3, 2, 2, 3, 'Wymiana wahaczy', '2020-05-02 12:00:00', '2020-05-12 10:00:00', 'Wymieniono wahacze', 'Wahacze tył Citroen Saxo', 123),
-(8, 1, 3, 2, 2, 3, 'Naprawa radia', '2020-05-04 09:00:00', '2020-05-08 14:00:00', 'Podłączono zasilanie', 'Olej silnikowy 5W40 5L - 250\nOlej silnikowy 5W40 5L - 250\nOlej silnikowy 5W40 5L - 250\n', 321);
+       (5, 4, 3, 2, NULL, 2, 'Niedziałający klakson', '2020-04-22 15:00:00', NULL, 'Naprawiono klakson',
+        'Olej silnikowy 5W40 5L - 250\nOlej silnikowy 5W40 5L - 250\nOlej silnikowy 5W40 5L - 250\n', NULL),
+       (6, 3, 3, 2, NULL, 2, 'Wymiana oleju silnikowego', '2020-04-28 13:00:00', NULL, 'Wymieniono olej silnikowy', '',
+        NULL),
+       (7, 2, 3, 2, 2, 3, 'Wymiana wahaczy', '2020-05-02 12:00:00', '2020-05-12 10:00:00', 'Wymieniono wahacze',
+        'Wahacze tył Citroen Saxo', 123),
+       (8, 1, 3, 2, 2, 3, 'Naprawa radia', '2020-05-04 09:00:00', '2020-05-08 14:00:00', 'Podłączono zasilanie',
+        'Olej silnikowy 5W40 5L - 250\nOlej silnikowy 5W40 5L - 250\nOlej silnikowy 5W40 5L - 250\n', 321);
